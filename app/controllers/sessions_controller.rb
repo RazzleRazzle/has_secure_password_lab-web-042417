bdcc1172cb0c16492be737d3306926854b4451e5
Jake Musich <makejusich@gmail.com>
2017-06-01 12:47:17 -0400

    done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:user][:name])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+    else
+      redirect_to signup_path 
+    end
+  end
+
+  def destroy
+    session.delete[:user_id]
+    redirect_to login_path
+  end
+end
